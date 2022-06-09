75a0ffc4e228dfcadd909d83949ba5a74db39342
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-06-09 18:19:08 -0300

    Adjust unit test

--- src/test/java/br/com/jamesson/coffee/CafeTest.java
+++ src/test/java/br/com/jamesson/coffee/CafeTest.java
@@ -7,16 +7,42 @@ public class CafeTest {
 
     @Test
     public void canBrewEspresso(){
+
+        // given
         Cafe cafe = new Cafe();
         cafe.restockBeans(7);
 
+        // when
         Coffee coffee = cafe.brew(CoffeeType.Espresso);
 
-        // no milk
-        // that we've got enough coffee
+        // then
         Assert.assertEquals(CoffeeType.Espresso, coffee.getType());
         Assert.assertEquals(0, coffee.getMilk());
         Assert.assertEquals(7, coffee.getBeans());
+    }
+
+    @Test
+    public void brewingEspressoConsumesBeans(){
+        // given
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7);
+
+        // when
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        // then
+        Assert.assertEquals(0, cafe.getBeansInStock());
+    }
+
+    // then
+    @Test(expected = IllegalStateException.class)
+    public void lattesRequiresMilk(){
+        // given
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7);
+
+        // when
+        cafe.brew(CoffeeType.Latte);
     }
 
 }
