7e3e4978852c04ac01940acbbf95335738317609
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-06-09 14:22:10 -0300

    Adding unit test

--- src/test/java/br/com/jamesson/coffee/CafeTest.java
+++ src/test/java/br/com/jamesson/coffee/CafeTest.java
@@ -1,12 +1,22 @@
 package br.com.jamesson.coffee;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CafeTest {
 
     @Test
     public void canBrewEspresso(){
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7);
 
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        // no milk
+        // that we've got enough coffee
+        Assert.assertEquals(CoffeeType.Espresso, coffee.getType());
+        Assert.assertEquals(0, coffee.getMilk());
+        Assert.assertEquals(7, coffee.getBeans());
     }
 
 }
